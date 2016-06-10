d51174678721048e1f7a05c2f3d537d42b550c36
Samuel Neves <sneves@dei.uc.pt>
2016-06-10 11:37:44 +0100

    fix kat paths; better sse2 detection

--- sse/blake2-config.h
+++ sse/blake2-config.h
@@ -1,14 +1,14 @@
 /*
    BLAKE2 reference source code package - optimized C implementations
-  
+
    Copyright 2012, Samuel Neves <sneves@dei.uc.pt>.  You may use this under the
    terms of the CC0, the OpenSSL Licence, or the Apache Public License 2.0, at
    your option.  The terms of these licenses can be found at:
-  
+
    - CC0 1.0 Universal : http://creativecommons.org/publicdomain/zero/1.0
    - OpenSSL license   : https://www.openssl.org/source/license.html
    - Apache 2.0        : http://www.apache.org/licenses/LICENSE-2.0
-  
+
    More information about the BLAKE2 hash function can be found at
    https://blake2.net.
 */
@@ -17,7 +17,7 @@
 #define __BLAKE2_CONFIG_H__
 
 // These don't work everywhere
-#if defined(__SSE2__)
+#if defined(__SSE2__) || defined(__x86_64__) || defined(__amd64__)
 #define HAVE_SSE2
 #endif
 
