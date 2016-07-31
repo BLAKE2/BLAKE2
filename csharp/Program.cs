5deb852c792213946f8ccdbf47af60d764639b7c
metadings <metadings@live.de>
2016-07-31 21:48:54 +0200

    New Blake2.cs: Inheriting HashAlgorithm and IDisposable

---
+++ csharp/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace Blake2
+{
+	internal static class Program
+	{
+		static void Main(string[] args) 
+		{
+			int argsI; // string command;
+			Dictionary<string, string> dictionary
+				= ReadConsoleArguments(args, out argsI); //, out command);
+
+			var concatArgs = string.Empty;
+			var concatArgsSB = new StringBuilder();
+			for (int j = 0; argsI < args.Length; ++argsI, ++j)
+			{
+				if (j > 0) concatArgsSB.Append(' ');
+				concatArgsSB.Append(args[argsI]);
+			}
+			concatArgs = concatArgsSB.ToString();
+
+			byte[] bytes = Encoding.UTF8.GetBytes(concatArgs);
+			byte[] value;
+
+			using (var hash = new Blake2B()) value = hash.ComputeHash(bytes);
+
+			foreach (byte v in value) Console.Write("{0:x2}", v);
+			Console.WriteLine();
+		}
+
+		static Dictionary<string, string> ReadConsoleArguments(string[] args, out int argsI) // , out string command)
+		{
+			argsI = 0;
+
+			int nameI, dashs;
+			string arg, argName;
+			var dictionary = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
+			do
+			{
+				if (args.Length == 0) break;
+
+				arg = args[argsI];
+				nameI = arg.IndexOf('=');
+				dashs = arg.StartsWith("--") ? 2 : (arg.StartsWith("-") ? 1 : 0);
+
+				if (dashs > 0)
+				{
+					if (nameI == -1)
+					{
+						argName = arg.Substring(dashs);
+						dictionary.Add(argName, string.Empty);
+						continue;
+					}
+					else
+					{
+						argName = arg.Substring(dashs, nameI - dashs);
+						arg = arg.Substring(nameI + 1);
+						dictionary.Add(argName, arg);
+						continue;
+					}
+				}
+
+				break;
+			} while (++argsI < args.Length);
+
+			/* command = string.Empty;
+			if (argsI < args.Length)
+			{
+				command = args[++argsI];
+			} /**/
+
+			return dictionary;
+		}
+	}
+}
+
--- csharp/README.md
+++ csharp/README.md
@@ -30,13 +30,11 @@ using (var hash = new Blake2B()) value = hash.ComputeHash(bytes);
 **Examples**
 
 ```
-metadings@metadings-mint ~/Blake2.cs/bin/Debug $ mono --debug ./Blake2.exe --Text=HelloWorldOfDonaldDuckAndMickeyM Test
-{    Text: 48656c6c6f576f726c644f66446f6e61 6c644475636b416e644d69636b65794d,
-  Blake2B: ff982bbcce1ff9d24ea1d9c06f0568cb 4c612053c93876cac0ab91b3ce00a5e3  b4708a146d41cb1e55e77a8863ce4229 8e22de0c34c85ee92dbcc9dab9092c6c }
+metadings@metadings-mint ~/Blake2.cs/bin/Debug $ mono ./Blake2.exe HelloWorldOfDonaldDuckAndMickeyM
+ff982bbcce1ff9d24ea1d9c06f0568cb4c612053c93876cac0ab91b3ce00a5e3b4708a146d41cb1e55e77a8863ce42298e22de0c34c85ee92dbcc9dab9092c6c
 
-metadings@metadings-mint ~/Blake2.cs/bin/Debug $ mono --debug ./Blake2.exe --Text=HHHHAAAALLLLOOOOWWWWEEEELLLLTTTT Test
-{    Text: 48484848414141414c4c4c4c4f4f4f4f 57575757454545454c4c4c4c54545454,
-  Blake2B: bbc9e82dbf9a8897a5ec2f6836c381db e27ac0b8ecd9912afa67459ef9474d70  a52bf24ad5dcf29dbb8004d19a387b65 16cc47ffae99d59d52efc013456c6b48 }
+metadings@metadings-mint ~/Blake2.cs/bin/Debug $ mono ./Blake2.exe HHHHAAAALLLLOOOOWWWWEEEELLLLTTTT
+bbc9e82dbf9a8897a5ec2f6836c381dbe27ac0b8ecd9912afa67459ef9474d70a52bf24ad5dcf29dbb8004d19a387b6516cc47ffae99d59d52efc013456c6b48
 ```
 
 Ask questions on [stackoverflow](http://stackoverflow.com/questions/tagged/c%23+blake2) using tags `C#``Blake2` !
