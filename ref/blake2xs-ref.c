c44e34b3a56209fe7870b154981dbf2f3d0842c2
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2016-10-11 22:00:39 +0200

    copyright notice

--- ref/blake2xs-ref.c
+++ ref/blake2xs-ref.c
@@ -1,3 +1,21 @@
+/*
+   BLAKE2 reference source code package - reference C implementations
+  
+   Copyright 2016, JP Aumasson <jeanphilippe.aumasson@gmail.com>.  
+   Copyright 2016, Samuel Neves <sneves@dei.uc.pt>.  
+   
+   You may use this under the terms of the CC0, the OpenSSL Licence, or
+   the Apache Public License 2.0, at your option.  The terms of these
+   licenses can be found at:
+  
+   - CC0 1.0 Universal : http://creativecommons.org/publicdomain/zero/1.0
+   - OpenSSL license   : https://www.openssl.org/source/license.html
+   - Apache 2.0        : http://www.apache.org/licenses/LICENSE-2.0
+  
+   More information about the BLAKE2 hash function can be found at
+   https://blake2.net.
+*/
+
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
